9a16404d03bfe9eec6c929fbe3d36e09d4fabe42
Nicholas Fazzolari <pdxpunk85@gmail.com>
2015-11-03 23:33:56 -0800

    Aligned the image overlay contents to center on X and Y

--- js/img-gallery.js
+++ js/img-gallery.js
@@ -3,14 +3,14 @@
 // Date: 11/2/2015
 
 function changeVis() {
-    document.getElementById("imgover").className = "gal-overlay-active";
+    document.getElementById("imgoverlay").className = "gal-overlay-active";
 }
 
 function changeHid() {
-    document.getElementById("imgover").className = "gal-overlay-hidden";
+    document.getElementById("imgoverlay").className = "gal-overlay-hidden";
 }
 
 // toggle visibility
 document.getElementById("tnsm").addEventListener('click', changeVis);
 
-document.getElementById("imgover").addEventListener('click', changeHid);
+document.getElementById("imgoverlay").addEventListener('click', changeHid);
