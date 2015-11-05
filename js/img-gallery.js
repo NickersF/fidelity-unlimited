7f1d92836d8f394a4e8a98c46f8de32c8afc8f48
Nicholas Fazzolari <pdxpunk85@gmail.com>
2015-11-04 23:54:11 -0800

    Began solving the automation of populating the grid with images.

--- js/img-gallery.js
+++ js/img-gallery.js
@@ -1,12 +1,14 @@
 // Purpose: Basic responsive image viewing controls
-// Author: Nicholas Fazzolari
+// Author: Nenad Pantic, Nicholas Fazzolari
 // Date: 11/2/2015
 
 function changeVis() {
+    'use strict';
     document.getElementById("imgoverlay").className = "gal-overlay-active";
 }
 
 function changeHid() {
+    'use strict';
     document.getElementById("imgoverlay").className = "gal-overlay-hidden";
 }
 
@@ -14,3 +16,34 @@ function changeHid() {
 document.getElementById("tnsm").addEventListener('click', changeVis);
 
 document.getElementById("imgoverlay").addEventListener('click', changeHid);
+
+// append divs to each row this function needs to be more extensible
+(function () {
+    'use strict';
+    //function body
+    function createDiv() {
+        var imageContainer = document.createElement("div");
+        
+        imageContainer.className = "col-md-3";
+        imageContainer.textContent = "Automatically Generated div Element";
+        
+        return imageContainer;
+    }
+    
+    function createAndModifyDivs() {
+        var row = document.getElementById("row1"), // replace with function argument value
+            newDivs = [],
+            i = 0,
+            numOfDivs = 4; // replace with argument value
+        
+        for (i; i < numOfDivs; i += 1) {
+            newDivs.push(createDiv());
+            row.appendChild(newDivs[i]);
+        }
+        
+        newDivs.textContent = "Edited Via Function";
+    }
+    
+    createAndModifyDivs();
+    
+}());
